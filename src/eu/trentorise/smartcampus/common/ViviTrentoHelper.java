f45b28a529601aa9a9bb8d53019be4f4de33ff6a
o.zambotti <o.zambotti@d7a6bff6-3310-458a-9e08-db4c87936053>
2013-03-08 18:01:34 +0000

    ViviTrento unified app update

---
+++ src/eu/trentorise/smartcampus/common/ViviTrentoHelper.java
@@ -0,0 +1,38 @@
+package eu.trentorise.smartcampus.common;
+
+import android.content.Context;
+import eu.trentorise.smartcampus.ac.SCAccessProvider;
+import eu.trentorise.smartcampus.ac.authenticator.AMSCAccessProvider;
+import eu.trentorise.smartcampus.notifications.NotificationsHelper;
+
+public class ViviTrentoHelper {
+
+	public static String APP_TOKEN = "vivitrento";
+	public static String SYNC_DB_NAME = "vivitrento_notifications";
+	public static String SYNC_SERVICE = "/communicator/sync";
+	public static String AUTHORITY = "eu.trentorise.smartcampus.notifications";
+
+	private static Context mContext;
+	private static ViviTrentoHelper helper;
+	
+	private static SCAccessProvider accessProvider;
+
+	public ViviTrentoHelper(Context ctx) {
+		this.mContext = ctx;
+	}
+
+	public static void init(Context ctx) {
+		if (helper == null) {
+			helper = new ViviTrentoHelper(ctx);
+		}
+		NotificationsHelper.init(ctx, APP_TOKEN, SYNC_DB_NAME, SYNC_SERVICE, AUTHORITY);
+	}
+
+	public static SCAccessProvider getAccessProvider() {
+		if (accessProvider == null) {
+			accessProvider = new AMSCAccessProvider();
+		}
+		
+		return accessProvider;
+	}
+}
---
+++ src/eu/trentorise/smartcampus/vivitrento/syncadapter/NotificationsSyncAdapter.java
@@ -0,0 +1,141 @@
+/*******************************************************************************
+ * Copyright 2012-2013 Trento RISE
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either   express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package eu.trentorise.smartcampus.vivitrento.syncadapter;
+
+import java.util.List;
+
+import android.accounts.Account;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SyncResult;
+import android.os.Bundle;
+import android.util.Log;
+import eu.trentorise.smartcampus.android.common.GlobalConfig;
+import eu.trentorise.smartcampus.common.ViviTrentoHelper;
+import eu.trentorise.smartcampus.communicator.model.Notification;
+import eu.trentorise.smartcampus.notifications.NotificationsHelper;
+import eu.trentorise.smartcampus.protocolcarrier.exceptions.SecurityException;
+import eu.trentorise.smartcampus.storage.sync.SyncData;
+import eu.trentorise.smartcampus.storage.sync.SyncStorage;
+import eu.trentorise.smartcampus.vivitrento.R;
+
+/**
+ * SyncAdapter implementation for syncing sample SyncAdapter contacts to the
+ * platform ContactOperations provider.
+ */
+public class NotificationsSyncAdapter extends AbstractThreadedSyncAdapter {
+	private static final String TAG = "NotificationsSyncAdapter";
+
+	private final Context mContext;
+
+	public NotificationsSyncAdapter(Context context, boolean autoInitialize) {
+		super(context, autoInitialize);
+		mContext = context;
+		ViviTrentoHelper.init(mContext);
+		try {
+			NotificationsHelper.start(true);
+		} catch (Exception e) {
+			Log.e(TAG, "Failed to instantiate SyncAdapter: " + e.getMessage());
+		}
+	}
+
+	@Override
+	public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider,
+			SyncResult syncResult) {
+		try {
+			Log.e(TAG, "Trying synchronization");
+			SyncStorage storage = NotificationsHelper.getSyncStorage();
+			SyncData data = storage.synchronize(NotificationsHelper.getAuthToken(), GlobalConfig.getAppUrl(mContext),
+					ViviTrentoHelper.SYNC_SERVICE);
+			if (data.getUpdated() != null && !data.getUpdated().isEmpty()
+					&& data.getUpdated().containsKey(Notification.class.getCanonicalName()))
+				onDBUpdate(data.getUpdated().get(Notification.class.getCanonicalName()));
+		} catch (SecurityException e) {
+			handleSecurityProblem();
+		} catch (Exception e) {
+			Log.e(TAG, "on PerformSynch Exception: " + e.getMessage());
+		}
+	}
+
+	private void handleSecurityProblem() {
+		Intent i = new Intent("eu.trentorise.smartcampus.START");
+		i.setPackage(mContext.getPackageName());
+
+		ViviTrentoHelper.getAccessProvider().invalidateToken(mContext, null);
+
+		NotificationManager mNotificationManager = (NotificationManager) mContext
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		int icon = R.drawable.launcher;
+		CharSequence tickerText = mContext.getString(eu.trentorise.smartcampus.ac.R.string.token_expired);
+		long when = System.currentTimeMillis();
+		CharSequence contentText = mContext.getString(eu.trentorise.smartcampus.ac.R.string.token_required);
+		PendingIntent contentIntent = PendingIntent.getActivity(mContext, 0, i, 0);
+
+		android.app.Notification notification = new android.app.Notification(icon, tickerText, when);
+		notification.flags |= android.app.Notification.FLAG_AUTO_CANCEL;
+		notification.setLatestEventInfo(mContext, tickerText, contentText, contentIntent);
+
+		mNotificationManager.notify(eu.trentorise.smartcampus.ac.Constants.ACCOUNT_NOTIFICATION_ID, notification);
+	}
+
+	private void onDBUpdate(List<Object> list) {
+		Intent i = new Intent("eu.trentorise.smartcampus.START");
+		i.setPackage(mContext.getPackageName());
+
+		NotificationManager mNotificationManager = (NotificationManager) mContext
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		int icon = R.drawable.launcher;
+
+		CharSequence tickerText = extractTitle(list);
+		long when = System.currentTimeMillis();
+		CharSequence contentText = extractText(list);
+		PendingIntent contentIntent = PendingIntent.getActivity(mContext, 0, i, 0);
+
+		android.app.Notification notification = new android.app.Notification(icon, tickerText, when);
+		notification.flags |= android.app.Notification.FLAG_AUTO_CANCEL;
+		notification.setLatestEventInfo(mContext, tickerText, contentText, contentIntent);
+
+		mNotificationManager.notify(eu.trentorise.smartcampus.ac.Constants.ACCOUNT_NOTIFICATION_ID, notification);
+	}
+
+	private CharSequence extractTitle(List<Object> list) {
+		return format(list, eu.trentorise.smartcampus.vivitrento.R.string.notification_title,
+				eu.trentorise.smartcampus.vivitrento.R.string.notification_title_multi);
+	}
+
+	private CharSequence extractText(List<Object> list) {
+		return format(list, eu.trentorise.smartcampus.vivitrento.R.string.notification_text,
+				eu.trentorise.smartcampus.vivitrento.R.string.notification_text_multi);
+	}
+
+	private CharSequence format(List<Object> list, int res, int resMulti) {
+		String txt = "";
+		if (list.size() == 1) {
+			String title = ((Notification) list.get(0)).getTitle();
+			txt = mContext.getString(eu.trentorise.smartcampus.vivitrento.R.string.notification_title) + " " + title;
+		} else
+			txt = list.size() + " "
+					+ mContext.getString(eu.trentorise.smartcampus.vivitrento.R.string.notification_title_multi);
+		return txt;
+	}
+}
