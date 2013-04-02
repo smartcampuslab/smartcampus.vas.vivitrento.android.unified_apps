7601004ba1aa0be66e365ee240c5c7a242270110
o.zambotti <o.zambotti@d7a6bff6-3310-458a-9e08-db4c87936053>
2013-04-02 07:48:42 +0000

    Removing bin and gen/*

--- gen/eu/trentorise/smartcampus/vivitrento/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package eu.trentorise.smartcampus.vivitrento;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
