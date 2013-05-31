5c47a2f6eda761977e823a13cdae206faf4daa68
o.zambotti <o.zambotti@trentorise.eu>
2013-05-31 16:31:51 +0200

    Now assets json files are used for parameters

---
+++ assets/params_dt.js
@@ -0,0 +1,76 @@
+/*
+ * POI CATEGORIES
+ * 1 Museums
+ * 2 Mobility
+ * 3 Parking
+ * 4 Offices
+ * 5 Theater
+ * 6 University
+ * 7 Accomodation
+ * 8 Libraries
+ * 9 Food
+ * 10 Drink
+ * 11 Cinemas
+ * 12 Family - Organizations
+ * 13 other POIs
+ * 
+ * EVENT CATEGORIES
+ * 1 Concerts
+ * 2 Happy hours
+ * 3 Movies
+ * 4 Parties
+ * 5 Seminars
+ * 6 Theaters
+ * 7 Exhibitions
+ * 8 Family
+ * 9 other events
+ * 
+ * STORY CATEGORIES
+ * 1 Leisure
+ * 2 Offices and Services
+ * 3 University
+ * 4 Culture
+ * 5 other stories
+ */
+
+{
+	"app_token" : "vivitrento",
+	"title" : "blabla",
+	"poi_categories" : [
+	    1,
+	    2,
+	    3,
+	    4,
+	    5,
+	    6,
+	    7,
+	    8,
+	    9,
+	    10,
+	    11,
+	    13
+	],
+	"events_categories" : [
+	    1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        9
+   ],
+   "story_categories" : [
+        1,
+        2,
+        4,
+        5
+	],
+	"exclude" : {
+		"source" : [
+	        "smartplanner-transitstops",
+	        "TrentinoFamiglia"
+	    ],
+	    "type" : "Comune"
+	}
+}
---
+++ assets/params_jp.js
@@ -0,0 +1,38 @@
+/*
+ * SMART CHECK OPTIONS
+ * 1 bus trento timetable
+ * 2 bus rovereto timetable
+ * 3 suburban timetable
+ * 4 train timetable
+ * 5 parking trento
+ * 6 parking rovereto
+ * 
+ * BROADCAST NOTIFICATIONS OPTIONS
+ * 1 bus trento delay
+ * 2 bus rovereto delay
+ * 3 bus suburban delay
+ * 4 train delay
+ * 5 accident
+ * 6 road works
+ * 7 strike
+ * 8 traffic jam
+ * 9 diversion
+ */
+
+{
+    "app_token": "journeyplanner",
+    "smartcheck_options": [
+        1,
+        4,
+        5
+    ],
+    "broadcast_notifications_options": [
+        1,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9
+    ]
+}
