614a22045356a8df318ce20027091864ec732b9b
raman <raman@fbk.eu>
2013-06-25 14:12:39 +0200

    Correct authority (use xml/strings)

--- src/eu/trentorise/smartcampus/common/ViviTrentoHelper.java
+++ src/eu/trentorise/smartcampus/common/ViviTrentoHelper.java
@@ -4,13 +4,13 @@ import android.content.Context;
 import eu.trentorise.smartcampus.ac.SCAccessProvider;
 import eu.trentorise.smartcampus.ac.authenticator.AMSCAccessProvider;
 import eu.trentorise.smartcampus.notifications.NotificationsHelper;
+import eu.trentorise.smartcampus.vivitrento.R;
 
 public class ViviTrentoHelper {
 
 	public static String APP_TOKEN = "vivitrento";
 	public static String SYNC_DB_NAME = "vivitrento_notifications";
 	public static String SYNC_SERVICE = "/communicator/sync";
-	public static String AUTHORITY = "eu.trentorise.smartcampus.notifications";
 
 	private static Context mContext;
 	private static ViviTrentoHelper helper;
@@ -25,7 +25,7 @@ public class ViviTrentoHelper {
 		if (helper == null) {
 			helper = new ViviTrentoHelper(ctx);
 		}
-		NotificationsHelper.init(ctx, APP_TOKEN, SYNC_DB_NAME, SYNC_SERVICE, AUTHORITY);
+		NotificationsHelper.init(ctx, APP_TOKEN, SYNC_DB_NAME, SYNC_SERVICE, ctx.getString(R.string.notificationsprovider_authority));
 	}
 
 	public static SCAccessProvider getAccessProvider() {
