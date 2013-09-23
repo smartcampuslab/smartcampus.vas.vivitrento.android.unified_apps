e33d59bd0b8c4bd34bb0e13bacce3835e18d93cb
chin8 <m.chini@trentorise.eu>
2013-09-23 10:16:14 +0200

    update widget

--- src/eu/trentorise/smartcampus/vivitrento/widget/shortcuts/ConfigurationActivity.java
+++
@@ -1,7 +0,0 @@
-package eu.trentorise.smartcampus.vivitrento.widget.shortcuts;
-
-import com.actionbarsherlock.app.SherlockFragmentActivity;
-
-public class ConfigurationActivity extends SherlockFragmentActivity{
-
-}
