6d076f08c98b7a277e5ae06b881acf395b7f3948
raman <raman@fbk.eu>
2013-09-26 11:00:50 +0200

    tutorial added, corrected auth management

--- src/eu/trentorise/smartcampus/viaggiatrento/LauncherActivity.java
+++ src/eu/trentorise/smartcampus/viaggiatrento/LauncherActivity.java
@@ -26,49 +26,43 @@ import android.content.res.Configuration;
 import android.content.res.Resources.NotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.SubMenu;
 
 import eu.trentorise.smartcampus.ac.Constants;
+import eu.trentorise.smartcampus.ac.SCAccessProvider;
 import eu.trentorise.smartcampus.ac.authenticator.AMSCAccessProvider;
 import eu.trentorise.smartcampus.android.common.GlobalConfig;
+import eu.trentorise.smartcampus.android.feedback.utils.FeedbackFragmentInflater;
 import eu.trentorise.smartcampus.common.ViviTrentoHelper;
-import eu.trentorise.smartcampus.dt.DiscoverTrentoActivity;
-import eu.trentorise.smartcampus.jp.BaseActivity;
-import eu.trentorise.smartcampus.jp.HomeActivity;
 import eu.trentorise.smartcampus.jp.MonitorJourneyActivity;
 import eu.trentorise.smartcampus.jp.PlanJourneyActivity;
 import eu.trentorise.smartcampus.jp.ProfileActivity;
 import eu.trentorise.smartcampus.jp.SavedJourneyActivity;
 import eu.trentorise.smartcampus.jp.SmartCheckActivity;
+import eu.trentorise.smartcampus.jp.TutorialManagerActivity;
 import eu.trentorise.smartcampus.jp.helper.JPHelper;
-import eu.trentorise.smartcampus.jp.helper.JPParamsHelper;
 import eu.trentorise.smartcampus.jp.notifications.BroadcastNotificationsActivity;
 import eu.trentorise.smartcampus.jp.notifications.NotificationsFragmentActivityJP;
-import eu.trentorise.smartcampus.viaggiatrento.R;
 import eu.trentorise.smartcampus.viaggiatrento.apps.ApkInstaller.ApkDownloaderTask;
 import eu.trentorise.smartcampus.viaggiatrento.util.ConnectionUtil;
 
-//public class LauncherActivity extends SherlockFragmentActivity {
-public class LauncherActivity extends BaseActivity {
+public class LauncherActivity extends TutorialManagerActivity {
 
 	public static final String UPDATE = "update";
-	private boolean token_present = false;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		JPHelper.init(getApplicationContext());
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.home);
 		try {
 			ViviTrentoHelper.init(getApplicationContext());
 
@@ -76,7 +70,6 @@ public class LauncherActivity extends BaseActivity {
 			initGlobalConstants();
 			//
 			if (accessprovider.readToken(this, null) == null) {
-				token_present = false;
 				// dialogbox for registration
 				DialogInterface.OnClickListener updateDialogClickListener;
 
@@ -85,9 +78,6 @@ public class LauncherActivity extends BaseActivity {
 					public void onClick(DialogInterface dialog, int which) {
 
 						try {
-							SharedPreferences settings = LauncherActivity.this.getSharedPreferences(
-									AppFragment.PREFS_NAME, 0);
-							SharedPreferences.Editor editor = settings.edit();
 							switch (which) {
 							case DialogInterface.BUTTON_POSITIVE:
 
@@ -106,8 +96,6 @@ public class LauncherActivity extends BaseActivity {
 
 								break;
 							}
-							editor.commit();
-							appFragmentCheckVersion();
 							invalidateOptionsMenu();
 
 						} catch (OperationCanceledException e) {
@@ -127,7 +115,10 @@ public class LauncherActivity extends BaseActivity {
 						.setPositiveButton(android.R.string.yes, updateDialogClickListener)
 						.setNegativeButton(R.string.not_now, updateDialogClickListener).show();
 			} else {
-				token_present = true;
+				if (JPHelper.isFirstLaunch(this)) {
+					showTourDialog();
+					JPHelper.disableFirstLaunch(this);
+				}
 			}
 
 		}
@@ -137,14 +128,10 @@ public class LauncherActivity extends BaseActivity {
 			finish();
 		}
 
-		// Getting saved instance
-		if (savedInstanceState == null) {
-			// Loading first fragment that works as home for application.
-			FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-			Fragment frag = new AppFragment();
-			ft.add(R.id.fragment_container, frag).commit();
+		// Feedback
+		FeedbackFragmentInflater.inflateHandleButtonInRelativeLayout(this,
+				(RelativeLayout) findViewById(R.id.home_relative_layout_jp));
 
-		}
 	}
 
 	public void goToFunctionality(View view) {
@@ -204,15 +191,8 @@ public class LauncherActivity extends BaseActivity {
 		Constants.setAuthUrl(this, getResources().getString(R.string.smartcampus_auth_url));
 		GlobalConfig.setAppUrl(this, getResources().getString(R.string.smartcampus_app_url));
 		SharedPreferences settings = LauncherActivity.this.getSharedPreferences(AppFragment.PREFS_NAME, 0);
-		SharedPreferences.Editor editor = settings.edit();
 		settings.edit().putBoolean(AppFragment.FIRSTTIME, true).commit();
 
-	}
-
-	private void appFragmentCheckVersion() {
-		AppFragment appfragment = (AppFragment) LauncherActivity.this.getSupportFragmentManager().findFragmentById(
-				R.id.fragment_container);
-		appfragment.check_version();
 	}
 
 	// getting the notification intent update the launcher
@@ -246,23 +226,15 @@ public class LauncherActivity extends BaseActivity {
 
 	}
 
-	public void launchDT(View v) {
-		startActivity(new Intent(this, HomeActivity.class));
-	}
-
-	public void launchJP(View v) {
-		startActivity(new Intent(this, DiscoverTrentoActivity.class));
-	}
-
 	@Override
 	protected void onResume() {
 		super.onResume();
 		if (JPHelper.isInitialized()) {
 			JPHelper.getLocationHelper().start();
 		}
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		Fragment frag = new AppFragment();
-		ft.add(R.id.fragment_container, frag).commit();
+//		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+//		Fragment frag = new AppFragment();
+//		ft.add(R.id.fragment_container, frag).commit();
 	}
 
 	@Override
@@ -281,35 +253,32 @@ public class LauncherActivity extends BaseActivity {
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == RESULT_OK) {
-			String token = data.getExtras().getString(AccountManager.KEY_AUTHTOKEN);
-			if (token == null) {
-				Toast.makeText(this, getString(R.string.auth_failed), Toast.LENGTH_SHORT).show();
+		if (requestCode == SCAccessProvider.SC_AUTH_ACTIVITY_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				String token = data.getExtras().getString(AccountManager.KEY_AUTHTOKEN);
+				if (token == null) {
+					Toast.makeText(this, getString(R.string.auth_failed), Toast.LENGTH_SHORT).show();
+					// clean shared preferences
+				} else {
+					invalidateOptionsMenu();
+					if (JPHelper.isFirstLaunch(this)) {
+						showTourDialog();
+						JPHelper.disableFirstLaunch(this);
+					}
+				}
+
+			} else if (resultCode == RESULT_CANCELED) {
+				Toast.makeText(this, getString(R.string.token_required), Toast.LENGTH_LONG).show();
 				// clean shared preferences
+				finish();
+
 			} else {
-				appFragmentCheckVersion();
-				invalidateOptionsMenu();
+				Toast.makeText(this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+				// clean shared preferences
+				finish();
 			}
-
-		} else if (resultCode == RESULT_CANCELED) {
-			Toast.makeText(this, getString(R.string.token_required), Toast.LENGTH_LONG).show();
-			// clean shared preferences
-			finish();
-
-		} else {
-			Toast.makeText(this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
-			// clean shared preferences
-			finish();
 		}
 		super.onActivityResult(requestCode, resultCode, data);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		menu.clear();
-		MenuInflater inflater = getSupportMenuInflater();
-		inflater.inflate(R.menu.emptymenu, menu);
-		return true;
 	}
 
 	@Override
@@ -320,327 +289,38 @@ public class LauncherActivity extends BaseActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle item selection
-		switch (item.getItemId()) {
-		case android.R.id.home:
+		if (item.getItemId() == android.R.id.home) {
 			onBackPressed();
+		} else if (item.getItemId() == R.id.upgrade_user_menu) {
+			// promote user
+			AMSCAccessProvider ac = new AMSCAccessProvider();
+			ac.promote(this, null, ac.readToken(this, null));
+		} else if (item.getItemId() == R.id.about) {
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+					Uri.parse(getString(R.string.smartcampus_url_credits)));
+			startActivity(browserIntent);		
 		}
+		
 		return super.onOptionsItemSelected(item);
 	}
 
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		menu.clear();
+		getSupportMenuInflater().inflate(R.menu.launchergripmenu, menu);
+		menu.getItem(0).setVisible(false);
+		SubMenu submenu = menu.getItem(1).getSubMenu();
+		submenu.clear();
+		submenu.setIcon(R.drawable.ic_action_overflow);
+
+		submenu.add(Menu.CATEGORY_SYSTEM, R.id.menu_item_tutorial, Menu.NONE, R.string.menu_tutorial);
+		submenu.add(Menu.CATEGORY_SYSTEM, R.id.about, Menu.NONE, R.string.about);// about
+																					// page
+
+		if (new AMSCAccessProvider().isUserAnonymous(this)) {
+			submenu.add(Menu.CATEGORY_SYSTEM, R.id.upgrade_user_menu, Menu.NONE, R.string.upgrade_user_menu);
+		}
+
+		return super.onPrepareOptionsMenu(menu);
+	}
 }
-// public class LauncherActivity extends FeedbackFragmentActivity {
-//
-// public static final String UPDATE = "update";
-// private boolean token_present = false;
-// private boolean mHiddenNotification;
-//
-//
-//
-// private void setHiddenNotification() {
-// try {
-// ApplicationInfo ai =
-// getPackageManager().getApplicationInfo(this.getPackageName(),
-// PackageManager.GET_META_DATA);
-// Bundle aBundle = ai.metaData;
-// mHiddenNotification = aBundle.getBoolean("hidden-notification");
-// } catch (NameNotFoundException e) {
-// mHiddenNotification = false;
-// Log.e(HomeActivity.class.getName(),
-// "you should set the hidden-notification metadata in app manifest");
-// }
-// if (mHiddenNotification) {
-// View notificationButton = findViewById(R.id.btn_notifications);
-// if (notificationButton != null)
-// notificationButton.setVisibility(View.GONE);
-// }
-// }
-//
-// @Override
-// protected void onStart() {
-// super.onStart();
-//
-// getSupportActionBar().setHomeButtonEnabled(false);
-// getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-// getSupportActionBar().setDisplayShowTitleEnabled(true);
-//
-// if (getSupportActionBar().getNavigationMode() !=
-// ActionBar.NAVIGATION_MODE_STANDARD) {
-// getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-// }
-// }
-//
-//
-// @Override
-// public boolean onOptionsItemSelected(MenuItem item) {
-// // Handle item selection
-// switch (item.getItemId()) {
-// case android.R.id.home:
-// onBackPressed();
-// }
-// return super.onOptionsItemSelected(item);
-// }
-// @Override
-// protected void onResume() {
-// super.onResume();
-//
-// JPHelper.init(this);
-// JPHelper.getLocationHelper().start();
-// }
-//
-// @Override
-// protected void onPause() {
-// super.onPause();
-// JPHelper.getLocationHelper().stop();
-// }
-//
-// @Override
-// public boolean onCreateOptionsMenu(Menu menu) {
-// menu.clear();
-// MenuInflater inflater = getSupportMenuInflater();
-// inflater.inflate(R.menu.emptymenu, menu);
-// return true;
-// }
-//
-// public void goToFunctionality(View view) {
-// Intent intent;
-// int viewId = view.getId();
-//
-// if (viewId == R.id.btn_planjourney) {
-// intent = new Intent(this, PlanJourneyActivity.class);
-// intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-// startActivity(intent);
-// return;
-// } else if (viewId == R.id.btn_broadcast) {
-// intent = new Intent(this, BroadcastNotificationsActivity.class);
-// intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-// startActivity(intent);
-// return;
-// } else if (viewId == R.id.btn_myprofile) {
-// intent = new Intent(this, ProfileActivity.class);
-// intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-// startActivity(intent);
-// return;
-// } else if (viewId == R.id.btn_monitorsavedjourney) {
-// intent = new Intent(this, SavedJourneyActivity.class);
-// intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-// startActivity(intent);
-// return;
-// } else if (viewId == R.id.btn_smart) {
-// intent = new Intent(this, SmartCheckActivity.class);
-// intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-// startActivity(intent);
-// return;
-// } else if (viewId == R.id.btn_notifications) {
-// intent = new Intent(this, NotificationsFragmentActivityJP.class);
-// intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-// startActivity(intent);
-// return;
-// } else {
-// Toast toast = Toast.makeText(getApplicationContext(), R.string.tmp,
-// Toast.LENGTH_SHORT);
-// toast.show();
-// return;
-// }
-// }
-//
-// public int getMainlayout() {
-// return Config.mainlayout;
-// }
-// @Override
-// public void onCreate(Bundle savedInstanceState) {
-// super.onCreate(savedInstanceState);
-// setContentView(R.layout.home);
-// if (getSupportActionBar().getNavigationMode() !=
-// ActionBar.NAVIGATION_MODE_STANDARD)
-// getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-// FeedbackFragmentInflater.inflateHandleButtonInRelativeLayout(this,
-// (RelativeLayout) findViewById(R.id.home_relative_layout_jp));
-// setHiddenNotification();
-// try {
-// ViviTrentoHelper.init(getApplicationContext());
-//
-// final AMSCAccessProvider accessprovider = new AMSCAccessProvider();
-// initGlobalConstants();
-// //
-// if (accessprovider.readToken(this, null) == null) {
-// token_present = false;
-// // dialogbox for registration
-// DialogInterface.OnClickListener updateDialogClickListener;
-//
-// updateDialogClickListener = new DialogInterface.OnClickListener() {
-// @Override
-// public void onClick(DialogInterface dialog, int which) {
-//
-// try {
-// SharedPreferences settings =
-// LauncherActivity.this.getSharedPreferences(AppFragment.PREFS_NAME, 0);
-// SharedPreferences.Editor editor = settings.edit();
-// switch (which) {
-// case DialogInterface.BUTTON_POSITIVE:
-//
-// // yes -> accessprovider.getAuthToken(this,
-// // null);-> shared preferences "registred" true
-//
-// accessprovider.getAuthToken(LauncherActivity.this, null);
-// break;
-//
-// case DialogInterface.BUTTON_NEGATIVE:
-// // no -> accessprovider.getAuthToken(this,
-// // "anonymous"); -> shared preferences
-// // "registred" true
-//
-// accessprovider.getAuthToken(LauncherActivity.this, "anonymous");
-//
-// break;
-// }
-// editor.commit();
-// appFragmentCheckVersion();
-// invalidateOptionsMenu();
-//
-// } catch (OperationCanceledException e) {
-// Toast.makeText(LauncherActivity.this, getString(R.string.token_required),
-// Toast.LENGTH_LONG).show();
-// finish();
-// } catch (Exception e) {
-// Toast.makeText(LauncherActivity.this, getString(R.string.auth_failed),
-// Toast.LENGTH_SHORT).show();
-// finish();
-// }
-// }
-// };
-// AlertDialog.Builder builder = new AlertDialog.Builder(this);
-// builder.setCancelable(false);
-// builder.setMessage(getString(R.string.auth_required))
-// .setPositiveButton(android.R.string.yes, updateDialogClickListener)
-// .setNegativeButton(R.string.not_now, updateDialogClickListener).show();
-// } else {
-// token_present = true;
-// }
-//
-// }
-//
-// catch (Exception e) {
-// Toast.makeText(this, getString(R.string.auth_failed),
-// Toast.LENGTH_SHORT).show();
-// finish();
-// }
-//
-// // // Getting saved instance
-// // if (savedInstanceState == null) {
-// // // Loading first fragment that works as home for application.
-// // FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-// // Fragment frag = new AppFragment();
-// // ft.add(R.id.fragment_container, frag).commit();
-// //
-// // }
-// }
-// //
-// private void initGlobalConstants() throws NameNotFoundException,
-// NotFoundException {
-// Constants.setAuthUrl(this,
-// getResources().getString(R.string.smartcampus_auth_url));
-// GlobalConfig.setAppUrl(this,
-// getResources().getString(R.string.smartcampus_app_url));
-// }
-//
-// private void appFragmentCheckVersion() {
-// // AppFragment appfragment = (AppFragment)
-// LauncherActivity.this.getSupportFragmentManager().findFragmentById(
-// // R.id.fragment_container);
-// // appfragment.check_version();
-// }
-//
-// // getting the notification intent update the launcher
-// @Override
-// public void onNewIntent(Intent arg0) {
-// super.onNewIntent(arg0);
-// Bundle extras = arg0.getExtras();
-//
-// if (extras != null) {
-// ApkDownloaderTask mDownloaderTask = new ApkDownloaderTask(this,
-// extras.getString("url"));
-//
-// if (ConnectionUtil.isConnected(ConnectionUtil.getConnectivityManager(this)))
-// {
-// // Checking url
-// if (!TextUtils.isEmpty(extras.getString("url"))) {
-// if (mDownloaderTask != null && !mDownloaderTask.isCancelled()) {
-// mDownloaderTask.cancel(true);
-// }
-// mDownloaderTask = new ApkDownloaderTask(this,
-// extras.getString(AppFragment.PARAM_URL));
-// mDownloaderTask.execute();
-// } else {
-// Log.d(AppFragment.class.getName(), "Empty url for download: " +
-// extras.getString(AppFragment.PARAM_NAME));
-// Toast.makeText(this, R.string.error_occurs, Toast.LENGTH_SHORT).show();
-// }
-// } else {
-// Toast.makeText(this, R.string.enable_connection, Toast.LENGTH_SHORT).show();
-// Intent intent = ConnectionUtil.getWifiSettingsIntent();
-// startActivity(intent);
-// }
-// }
-//
-// }
-//
-//
-//
-//
-// @Override
-// public void onConfigurationChanged(Configuration newConfig) {
-// super.onConfigurationChanged(newConfig);
-//
-// }
-//
-// @Override
-// protected void onActivityResult(int requestCode, int resultCode, Intent data)
-// {
-// if (resultCode == RESULT_OK) {
-// String token = data.getExtras().getString(AccountManager.KEY_AUTHTOKEN);
-// if (token == null) {
-// Toast.makeText(this, getString(R.string.auth_failed),
-// Toast.LENGTH_SHORT).show();
-// // clean shared preferences
-// } else {
-// appFragmentCheckVersion();
-// invalidateOptionsMenu();
-// }
-//
-// } else if (resultCode == RESULT_CANCELED ) {
-// Toast.makeText(this, getString(R.string.token_required),
-// Toast.LENGTH_LONG).show();
-// // clean shared preferences
-// finish();
-//
-// }else {
-// Toast.makeText(this, getString(R.string.auth_failed),
-// Toast.LENGTH_LONG).show();
-// // clean shared preferences
-// finish();
-// }
-// super.onActivityResult(requestCode, resultCode, data);
-// }
-//
-//
-//
-// @Override
-// protected void onSaveInstanceState(Bundle arg0) {
-// super.onSaveInstanceState(arg0);
-// }
-//
-//
-//
-// @Override
-// public String getAppToken() {
-// // TODO Auto-generated method stub
-// return null;
-// }
-//
-// @Override
-// public String getAuthToken() {
-// // TODO Auto-generated method stub
-// return null;
-// }
-//
-// }
