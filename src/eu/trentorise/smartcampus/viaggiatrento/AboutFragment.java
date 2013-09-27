2f3185e7cfa7bcc62a1c47a89c443f5a6983dd74
raman <raman@fbk.eu>
2013-09-27 16:26:32 +0200

    About page, intent filters

--- src/eu/trentorise/smartcampus/viaggiatrento/AboutFragment.java
+++ src/eu/trentorise/smartcampus/viaggiatrento/AboutFragment.java
@@ -1,27 +1,35 @@
 package eu.trentorise.smartcampus.viaggiatrento;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
+import android.view.Window;
 
-import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.app.SherlockDialogFragment;
 
-import eu.trentorise.smartcampus.viaggiatrento.R;
-
-public class AboutFragment extends SherlockFragment {
+public class AboutFragment extends SherlockDialogFragment {
 
 	@Override
-	public void onCreate(Bundle arg0) {
-		super.onCreate(arg0);
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		Dialog dlg = super.onCreateDialog(savedInstanceState);
+		dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		return dlg;
 	}
-	
-	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent,	Bundle args) {
-		View v = inflater.inflate(R.layout.about, null);
-		// Getting UI references
-		return v;
+		return inflater.inflate(R.layout.about, null);
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		getSherlockActivity().getSupportActionBar().show();
+	}
+	@Override
+	public void onResume() {
+		super.onResume();
+		getSherlockActivity().getSupportActionBar().hide();
 	}
 }
