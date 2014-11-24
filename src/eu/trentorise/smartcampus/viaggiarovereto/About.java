28231ff576ef1871188341f5da3527c02afc3760
o.zambotti <o.zambotti@trentorise.eu>
2014-11-24 18:22:10 +0100

    Credits fixed!

--- src/eu/trentorise/smartcampus/viaggiarovereto/About.java
+++ src/eu/trentorise/smartcampus/viaggiarovereto/About.java
@@ -1,47 +1,70 @@
 package eu.trentorise.smartcampus.viaggiarovereto;
 
-import eu.trentorise.smartcampus.jp.BaseActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import eu.trentorise.smartcampus.jp.BaseActivity;
 
-public class About extends BaseActivity
-{
+public class About extends BaseActivity {
 	ImageButton close;
 	Fragment fragment;
-	
+
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_about);
 		getSupportActionBar().hide();
 		close = (ImageButton) findViewById(R.id.close_credits);
 		setBtClose();
 	}
-	
-	
-	private void setBtClose()
-	{
-		close.setOnClickListener(new OnClickListener()
-		{
 
+	private void setBtClose() {
+		close.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View arg0)
-			{
+			public void onClick(View arg0) {
 				onBackPressed();
 			}
 		});
 	}
-	
-	@Override
-	public void onBackPressed()
-	{
-		finish();
-		overridePendingTransition(R.anim.alpha_in,
-				R.anim.alpha_out);
 
+	@Override
+	public void onBackPressed() {
+		finish();
+		overridePendingTransition(R.anim.alpha_in, R.anim.alpha_out);
 	}
+
+	public void onClickFBK(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.fbk.eu"));
+		startActivity(i);
+	}
+	
+	public void onClickSC(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.smartcampuslab.it"));
+		startActivity(i);
+	}
+	
+	public void onClickComuneRovereto(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.comune.rovereto.tn.it"));
+		startActivity(i);
+	}
+	
+	public void onClickIEC(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.iescities.eu/"));
+		startActivity(i);
+	}
+	
+	public void onClickStreetlife(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.streetlife-project.eu/index.html"));
+		startActivity(i);
+	}
+	
 }
