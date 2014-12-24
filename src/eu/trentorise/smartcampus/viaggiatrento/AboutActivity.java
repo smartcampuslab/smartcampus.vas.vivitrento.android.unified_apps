6cf1c7d50c424cb2dc777f23d872c71074ee9008
raman <raman@fbk.eu>
2014-12-24 16:52:16 +0100

    new data and server

--- src/eu/trentorise/smartcampus/viaggiatrento/AboutActivity.java
+++ src/eu/trentorise/smartcampus/viaggiatrento/AboutActivity.java
@@ -1,8 +1,13 @@
 package eu.trentorise.smartcampus.viaggiatrento;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
@@ -20,12 +25,69 @@ public class AboutActivity extends SherlockActivity {
 				finish();
 			}
 		});
+		
+		try {
+			PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
+			TextView creditsVersion = (TextView) findViewById(R.id.credits_version);
+			creditsVersion.setText(info.versionName);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
 		this.overridePendingTransition(android.R.anim.fade_in, R.anim.push_down);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		finish();
+		overridePendingTransition(R.anim.pull_up, android.R.anim.fade_out);
+	}
+
+	public void onClickFBK(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.fbk.eu"));
+		startActivity(i);
+	}
+
+	public void onClickSC(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.smartcampuslab.it"));
+		startActivity(i);
+	}
+
+	public void onClickComune(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.comune.trento.it"));
+		startActivity(i);
+	}
+
+	public void onClickIEC(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.iescities.eu/"));
+		startActivity(i);
+	}
+
+	public void onClickStreetlife(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.streetlife-project.eu/index.html"));
+		startActivity(i);
+	}
+
+	public void onClickSwAbout(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://www.smartcampuslab.it/swabout"));
+		startActivity(i);
+	}
+
+	public void onClickCCT(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse("http://http://www.comunitrentini.it/"));
+		startActivity(i);
 	}
 
 }
